20e2564defad26e0d34f4871a8b7233c924ed080
Priya Singh <psingh90897@gmail.com>
2025-08-24 15:05:35 +0530

    Listing the products on the homescreen

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,16 @@
 import { Navbar, Nav, Container, NavLink } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-import React from "react";
+import logo from "../assets/logo.png";
 
 export const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">Proshop</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="ProShop"></img>
+            Proshop
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
