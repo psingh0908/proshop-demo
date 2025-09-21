bff3d67d05f1f672fb857a0d3d727470ecfe09b3
Priya Singh <psingh90897@gmail.com>
2025-09-21 10:31:35 +0530

    backend code

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,15 +1,15 @@
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import products from "../products";
+// import products from "../products";
 import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return <Card className="my-3 p3 rounded">
-    <Link to={`/product/${products._id}`}>
+    <Link to={`/product/${product._id}`}>
     <Card.Img src={product.image} variant="top"/>
      </Link>
     <Card.Body>
-        <Link to={`/product/${products._id}`}>
+        <Link to={`/product/${product._id}`}>
         <Card.Title as="Div">
         <strong>{product.name}</strong>
         </Card.Title>
