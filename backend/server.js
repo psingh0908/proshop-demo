d04f5f9cd992b4030d33a5592b34e5a9c261f873
Priya Singh <psingh90897@gmail.com>
2025-09-25 12:35:34 +0530

    Db connection code

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectdb from './config/db.js'
 const port = process.env.PORT || 5000;
 
+connectdb(); //Connect Mongodb
 const app = express();
 
 app.get('/', (req, res) => {
