4eea26f069b892d43a46c13fa95cba7e123d9957
Priya Singh <psingh90897@gmail.com>
2026-01-26 15:40:26 +0530

    L24-Fetching data from database

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,19 @@
-import express from 'express';
-import products from './data/products.js';
-import dotenv from 'dotenv';
+import express from "express";
+
+import dotenv from "dotenv";
 dotenv.config();
-import connectdb from './config/db.js'
+import connectdb from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+
 const port = process.env.PORT || 5000;
 
 connectdb(); //Connect Mongodb
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("API is running")
+app.get("/", (req, res) => {
+  res.send("API is running");
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-})
+app.use("/api/products", productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product);
-})
-
-app.listen(port, () => console.log(`Server is running on port ${port} !`))
+app.listen(port, () => console.log(`Server is running on port ${port} !`));
